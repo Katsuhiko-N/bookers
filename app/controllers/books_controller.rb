8f2a33335e86d999496f6853a9516938fc7da788
EC2 Default User <ec2-user@ip-172-31-36-57.ap-northeast-1.compute.internal>
2024-07-14 09:34:12 +0000

    [Add]一覧兼投稿ページ概成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,14 +2,19 @@ class BooksController < ApplicationController
   # 新規投稿画面
   def new
     # フォーム作成用のインスタンス変数
-    @book = Book.new
+    @book_new = Book.new
   end
 
 # 投稿機能
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to '/'
+    # 投稿成否のフラッシュメッセージ実装
+    if book.save
+      flash[:notice] ="a"
+      redirect_to '/'
+    else
+      render :new
+    end
   end
 
 
@@ -17,6 +22,8 @@ class BooksController < ApplicationController
 # 投稿一覧画面
   def index
     @books = Book.all
+    # 投稿機能用
+    @book_new = Book.new
   end
 
 
